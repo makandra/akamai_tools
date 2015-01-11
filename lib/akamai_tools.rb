79f3f9f37fca2538b8323386232b6834ac7eb786
Timo Schilling <timo@schilling.io>
2015-01-11 10:09:47 +0100

    logger support

--- lib/akamai_tools.rb
+++ lib/akamai_tools.rb
@@ -4,12 +4,23 @@ require "akamai_tools/upload"
 
 module AkamaiTools
   mattr_accessor :logging
+  mattr_writer :logger
   self.logging = true
 
   def self.log(type, message = nil)
     return unless self.logging
 
-    puts "AkamaiTools #{type}:\n#{message}\n"
+    self.logger.info "AkamaiTools #{type}:\n#{message}\n"
+  end
+
+  def self.logger
+    @@logger ||= AkamaiTools::Logger.new
+  end
+
+  class Logger
+    def info(message)
+      puts message
+    end
   end
 end
 
